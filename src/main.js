a7a3e4ccfa15cf584f6a347f43967e2cdd297dc3
Pruthvi Paghdal <pruthvi.paghdal@ge.com>
2018-05-03 11:25:52 +0530

    routing with react

--- src/main.js
+++ src/main.js
@@ -1,42 +1,49 @@
-$ = jQuery = require('jquery');
+
 var React = require('react');
-var Home = require('./components/homePage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
-(function (win) {
-  "use strict";
+var Router = require('react-router');
+var routes = require('./routes');
 
-  var App = React.createClass({
-	render: function () {
-	  var Child;
-console.log('route',this.props.route)
-	  switch (this.props.route) {
-		case 'about':
-		  Child = About;
-		  console.log('case','ab');
-		  break;
-		default:
-		  Child = Home;
-		  console.log('case',Child);
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('app'));
+});
 
-		  break;
-	  }
-
-	  return (
-	    <div>
-		<Header/>
-		<Child/>
-	  </div>
-	  );
-	}
-  });
-
-  function render() {
-	var route = win.location.hash.substr(1);
-	React.render(<App route={route}/>, document.getElementById('app'));
-  }
-
-  win.addEventListener('hashchange', render);
-  render();
-  React.render(<Home/>, document.getElementById('app'));
-})(window);
+// var Home = require('./components/homePage');
+// var About = require('./components/about/aboutPage');
+// var Header = require('./components/common/header');
+// (function (win) {
+//   "use strict";
+//
+//   var App = React.createClass({
+// 	render: function () {
+// 	  var Child;
+// console.log('route',this.props.route)
+// 	  switch (this.props.route) {
+// 		case 'about':
+// 		  Child = About;
+// 		  console.log('case','ab');
+// 		  break;
+// 		default:
+// 		  Child = Home;
+// 		  console.log('case',Child);
+//
+// 		  break;
+// 	  }
+//
+// 	  return (
+// 	    <div>
+// 		<Header/>
+// 		<Child/>
+// 	  </div>
+// 	  );
+// 	}
+//   });
+//
+//   function render() {
+// 	var route = win.location.hash.substr(1);
+// 	React.render(<App route={route}/>, document.getElementById('app'));
+//   }
+//
+//   win.addEventListener('hashchange', render);
+//   render();
+//   React.render(<Home/>, document.getElementById('app'));
+// })(window);
